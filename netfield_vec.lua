5ff00ce309cfd675c56ca44d367d420acdec2e63
Christopher Moore <christopher.e.moore@gmail.com>
2025-06-01 23:03:01 +0800

    getting rid o the ast for now, don't do auto inlining or anything

--- netfield_vec.lua
+++ netfield_vec.lua
@@ -1,9 +1,10 @@
+local template = require 'template'
 local table = require 'ext.table'
-local ast = require 'parser.lua.ast'
-local NetField = require 'netrefl.netfield'.NetField
 local vec2 = require 'vec.vec2'
 local vec3 = require 'vec.vec3'
 local vec4 = require 'vec.vec4'
+local ast = require 'parser.lua.ast'
+local NetField = require 'netrefl.netfield'.NetField
 
 -- ast.exec env, for vec table access
 local env = setmetatable({}, {__index=_G})
@@ -21,6 +22,7 @@ for dim=2,4 do
 	resultClasses[netclassname] = nc
 	env[netclassname] = nc
 
+--[===[ if you want the AST for inlining for perf gains ... i'm gonna sideline this for a while ...
 	do
 		local exprs = table()
 		for i=1,dim do
@@ -35,7 +37,7 @@ for dim=2,4 do
 			ast._return(
 				ast._concat(exprs:unpack())
 		))
---DEBUG:print(classname, '__netencode', nc.func__netencode:toLua())
+print(classname, '__netencode', nc.func__netencode:toLua())
 		assert(nc.func__netencode:exec(nil, nil, env))()
 	end
 
@@ -53,7 +55,7 @@ for dim=2,4 do
 				ast._call(ast._var(classname),
 					exprs:unpack()
 		)))
---DEBUG:print(classname, '__netparse', nc.func__netparse:toLua())
+print(classname, '__netparse', nc.func__netparse:toLua())
 		assert(nc.func__netparse:exec(nil, nil, env))()
 	end
 
@@ -76,10 +78,50 @@ for dim=2,4 do
 			{ast._arg(),ast._arg()},	-- src, body
 			table.unpack(stmts)
 		)
---DEBUG:print(classname, '__netcopy', nc.func__netcopy:toLua())
+print(classname, '__netcopy', nc.func__netcopy:toLua())
 		assert(nc.func__netcopy:exec(nil, nil, env))()
 	end
-
+--]===]
+-- [===[ for the time being...
+	local code = template([[
+function <?=netclassname?>.__netencode(arg1)
+	return <?
+for i=1,dim do
+	if i > 1 then ?>.." "..<? end
+	?>arg1[<?=i?>]<?
+end
+?>
+end
+function <?=netclassname?>.__netparse(arg1)
+<?
+for i=1,dim do
+?>	local x<?=i?> = tonumber(arg1:next())
+<?
+end
+?>
+	return <?=classname?>(<?
+for i=1,dim do
+	if i > 1 then ?>, <? end
+	?>x<?=i?><?
+end
+?>)
+end
+function <?=netclassname?>.__netcopy(arg1, arg2)
+	arg2 = arg2 or <?=classname?>()
+<?
+for i=1,dim do
+?>	arg2[<?=i?>] = arg1[<?=i?>]
+<? end
+?>	return arg2
+end
+]], {
+	dim = dim,
+	classname = classname,
+	netclassname = netclassname,
+})
+--DEBUG:print(classname, code)
+	assert(load(code, nil, nil, env))()
+--]===]
 	-- should be the same as not a == b ?
 	nc.__netdiff = function(a,b) return a ~= b end
 	--nc.__netsend = NetField.__netsend	-- inherit from parent
